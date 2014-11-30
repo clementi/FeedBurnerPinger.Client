4481113e812444cb96b354804e6bbec2910b5237
Jeff Pratt <me@jeffreypratt.net>
2014-11-29 22:29:25 -0700

    Make DLL use correct configuration

--- FeedBurnerPinger.Client/PingingClient.cs
+++ FeedBurnerPinger.Client/PingingClient.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Specialized;
+﻿using System;
+using System.Collections.Specialized;
 using System.Configuration;
 using System.IO;
 using System.Runtime.CompilerServices;
@@ -10,6 +11,8 @@ namespace FeedBurnerPinger.Client
 {
     public class PingingClient : IPingingClient
     {
+        private const string EndpointUrlSettingKey = "EndpointUrl";
+        private const string UrlFormParameterName = "url";
         private readonly string endpointUrl;
         private readonly IWebClient webClient;
         private readonly JsonSerializer serializer;
@@ -21,16 +24,40 @@ namespace FeedBurnerPinger.Client
             this.serializer = new JsonSerializer();
         }
 
-        public PingingClient() : this(ConfigurationManager.AppSettings["EndpointUrl"], new SystemWebClient())
+        public PingingClient() : this(GetEndpointUrl(), new SystemWebClient())
         {
         }
 
         public PingResponse Ping(PingRequest request)
         {
-            byte[] response = this.webClient.UploadValues(this.endpointUrl, new NameValueCollection { { "url", request.FeedUrl } });
+            byte[] response = this.webClient.UploadValues(this.endpointUrl, new NameValueCollection { { UrlFormParameterName, request.FeedUrl } });
          
             using (var reader = new StreamReader(new MemoryStream(response)))
                 return this.serializer.Deserialize<PingResponse>(new JsonTextReader(reader));
+        }
+
+        private static string GetConfigurationAppSetting(Configuration config, string key)
+        {
+            if (config != null)
+            {
+                KeyValueConfigurationElement element = config.AppSettings.Settings[key];
+
+                if (element != null)
+                {
+                    string value = element.Value;
+                    if (!string.IsNullOrWhiteSpace(value))
+                        return value;
+                }
+            }
+            return string.Empty;
+        }
+
+        private static string GetEndpointUrl()
+        {
+            string exeConfigPath = typeof(PingingClient).Assembly.Location;
+            Configuration config = ConfigurationManager.OpenExeConfiguration(exeConfigPath);
+
+            return GetConfigurationAppSetting(config, EndpointUrlSettingKey);
         }
     }
 }
